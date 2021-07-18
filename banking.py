bc3834395cff5e87e956bf3bdd4ae4240a6599fd
Zoe Holubkova <zzoehol@gmail.com>
2021-07-18 15:36:34 +0300

    Implement Luhn algorithm for checking card number validity

--- banking.py
+++ banking.py
@@ -1,11 +1,34 @@
 import random
 
 
+def generate_card_number():
+    account_id = str(random.randint(0, 99999999))
+    card_number = '400000' + '0' * (9 - int(len(account_id))) + account_id
+
+    summa = 0
+    for ind, n in enumerate(card_number):
+        if ind % 2 == 0:
+            n = int(n) * 2
+
+        if int(n) > 9:
+            n = int(n) - 9
+
+        summa += int(n)
+
+    checksum = str(10 - summa % 10)
+    if checksum == '10':
+        checksum = '0'
+    print(summa)
+    print(summa % 10)
+    print(checksum)
+    return card_number + checksum
+
+
 def create_account():
-    account_id = str(random.randint(0, 9999999999))
-    card_number = '400000' + '0' * (10 - int(len(account_id))) + account_id
+    card_number = generate_card_number()
     card_pin = str(random.randint(0, 9999))
     card_pin = '0' * (4 - int(len(card_pin))) + card_pin
+
     bank_db[card_number] = {
         "pin": card_pin,
         "balance": 0.0
